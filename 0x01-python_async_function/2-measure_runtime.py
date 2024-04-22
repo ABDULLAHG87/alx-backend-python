8466366db120cc90e2c9e1c018a381050acfebec
root <hakeemabdullah87@gmail.com>
2024-04-22 15:00:37 -0700

    BUGFIX: spelling error in importation"

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-"""
-Create a measure_time function with integers n and max_delay as arguments that
-measures the total execution time for wait_n(n, max_delay), and returns
+"""Create a measure_time function with integers n and max_delay as arguments
+that measures the total execution time for wait_n(n, max_delay), and returns
 total_time / n. Your function should return a float
 """
 
 
 import time
 import asyncio
-wait_n = __import__('1-current_coroutines').wait_n
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
