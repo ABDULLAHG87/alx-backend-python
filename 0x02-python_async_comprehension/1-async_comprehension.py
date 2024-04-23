8aebd7c7bf39be19f8552a28259dc55b6bd3490f
root <hakeemabdullah87@gmail.com>
2024-04-23 13:42:54 -0700

    BUGFIX: make changes to the import section of the code

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,7 +4,7 @@ called async_comprehension that takes no arguments.
 """
 
 from typing import List
-async_generator = __import___('0-async_generator').async_generator
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
