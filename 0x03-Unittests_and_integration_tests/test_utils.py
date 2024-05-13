ba5cb2665b2045c281e3f1addc533858d523d7ae
Abdullahi <hakeemabdullah87@gmail.com>
2024-05-13 00:05:28 +0000

    FEAT: updating code for task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -35,3 +35,52 @@ class TestAccessNestedMap(unittest.TestCase):
         """method to test access_nested_map exception"""
         with self.assertRaises(expected_output) as context:
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """A class to test Get Json using unittest"""
+    @parameterized.expand(
+        [
+            ('http://example.com', {'payload': True}),
+            ('http://holberton.io', {'payload': False})
+        ]
+    )
+    def test_get_json(self, url, expected_output):
+        """Method for testing get json"""
+        mock_response = Mock()
+        mock_response.json.return_value = expected_output
+        with patch('request.get', return_value=mock_response):
+            response = get_json(url)
+
+            self.assertEqual(response, expected_output)
+
+
+class TestMemoize(unittest.TestCase):
+    """Class for testing Memoize
+    """
+    def test_memoize(self):
+        """Method for testing memoize"""
+
+        class TestClass:
+            """Test cases
+            """
+            def a_method(self):
+                """method a"""
+                return 42
+
+            @memoize
+            def a_property(self):
+                """method for property in memoize"""
+                return self.a_method()
+
+    test_object = TestClass()
+
+    with patch.object(test_obj, 'a_method') as mock_method:
+        mock_method.return_value = 42
+
+        result1 = test_obj.a_property
+        result2 = test_obj.a_property
+
+        self.assertEqual(result1, 42)
+        self.assertEqual(result2, 42)
+        mock_method.assert_called_once()
